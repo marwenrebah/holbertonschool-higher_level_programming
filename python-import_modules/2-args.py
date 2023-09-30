0e7fd244ebe0176f53a3412da9b49b4e6e5b9418
marwenrebah <6863@holbertonstudents.com>
2023-09-30 07:11:49 -1200

    script dynamic!

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-if __name__ != "__main__":
-    exit()
-
-argStr = "{:d} argument"
-argc = len(sys.argv) - 1
-if argc == 0:
-    argStr += 's.'
-elif argc == 1:
-    argStr += ':'
+count = len(sys.argv) - 1
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:")
 else:
-    argStr += 's:'
-print(argStr.format(argc))
-
-i = 0
-for arg in sys.argv:
-    if i != 0:
-        print("{:d}: {:s}".format(i, arg))
-        i += 1
+    print("{} arguments:".format (count))
+for i in range(count):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
