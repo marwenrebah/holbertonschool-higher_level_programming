712caae2e050751543e0fc34368467e1f7a64818
marwenrebah <6863@holbertonstudents.com>
2023-09-30 07:32:10 -1200

    Everything can be imported

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from variable_load_5.py import a
 
 if __name__ == "__main__":
-    exit()
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
 
-print("{:d}".format(a))
+    print(a)
