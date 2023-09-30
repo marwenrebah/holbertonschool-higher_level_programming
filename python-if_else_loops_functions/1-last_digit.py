3142daa997ad5526c3b8c8f7b43dbb4e5122b042
marwenrebah <6863@holbertonstudents.com>
2023-09-29 20:10:02 -1200

    The last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
 mod = number % 10 if number > 10 else number % -10
 print("Last digit of {:d} is {:d} and is".format(number, mod), end="")
