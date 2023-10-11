786da66f0a5db432b679562d0e421e3210af5aad
marwenrebah <6863@holbertonstudents.com>
2023-10-11 01:51:39 -1100

    Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square generation module
+"""
+
+
+class Square:
+    """class defined for square generation
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.__size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
