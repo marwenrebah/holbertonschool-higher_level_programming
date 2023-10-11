16b0d7cf59eefe431a2f0e58a7e7389c02252903
marwenrebah <6863@holbertonstudents.com>
2023-10-11 00:38:56 -1100

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square generation module
+"""
+
+
+class Square:
+    """class defined for square generation
+    """
+    def __init__(self, size):
+        self.__size = size
