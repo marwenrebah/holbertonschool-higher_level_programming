33bb62118250f4ec6146025a3c9ded41ddf8d437
marwenrebah <6863@holbertonstudents.com>
2023-10-20 11:36:07 -1100

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""Module that loads, adds and saves arguments to a Python list"""
-from sys import argv
+"""Add all arguments to a Python list and save them to a file."""
+import sys
+import json
 
 
-load_file = __import__('6-load_from_json_file').load_from_json_file
-save_file = __import__('5-save_to_json_file').save_to_json_file
+def save_to_json_file(my_obj, filename):
+    """Save object to a JSON file."""
+    with open(filename, mode="w") as f:
+        json.dump(my_obj, f)
 
-try:
-    json_list = load_file('add_item.json')
-except (ValueError, FileNotFoundError):
-    json_list = []
 
-for item in argv[1:]:
-    json_list.append(item)
+def load_from_json_file(filename):
+    """Load object from a JSON file."""
+    with open(filename, mode="r") as f:
+        return json.load(f)
 
-save_file(json_list, 'add_item.json')
+
+if __name__ == "__main":
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
