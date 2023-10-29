de5f0b334651df990d66ebfb362cf870ba013ea2
marwenrebah <6863@holbertonstudents.com>
2023-10-29 09:05:06 -1100

    fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,12 +5,12 @@ Module with a Base class
 
 
 class Base:
-    """ Class Base """
+    """Class Base"""
     __nb_objects = 0
 
 
 def __init__(self, id=None):
-    """ Initializes instances """
+    """Initializes instances of the Base class"""
     if id is None:
         Base.__nb_objects += 1
         self.id = Base.__nb_objects
