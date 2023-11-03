91c8f2a0ef4af174f90a4daf8adf5afde4f773bc
marwenrebah <6863@holbertonstudents.com>
2023-11-02 22:41:04 -1100

    Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""Square class"""
-
+"""Write the class Square that inherits from Rectangle"""
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """square inherits from Rectangle"""
+    """this is the square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """instantiation"""
+        """use all attributes of Rectangle"""
         super().__init__(size, size, x, y, id)
 
+    # Return a string representation of the object
     def __str__(self):
-        """string representation"""
+        """[Square] (<id>) <x>/<y> - <size>"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
 
     # Getter and setter methods for the 'size' property
     @property
     def size(self):
-        """get_method"""
+        """retrieve the value of size"""
         return self.width
 
     @size.setter
     def size(self, size):
-        """set_method"""
+        """assign size to width and height"""
         if not isinstance(size, int):
             raise TypeError("width must be an integer")
         elif size <= 0:
@@ -36,32 +36,27 @@ class Square(Rectangle):
 
     # Update the attributes of the square using either args or kwargs
     def update(self, *args, **kwargs):
-        """Update"""
-
-        if args and len(args) != 0:
-            for idx in range(len(args)):
-                if not idx:
-                    self.id = args[idx]
-                elif idx == 1:
-                    self.size = args[idx]
-                elif idx == 2:
-                    self.x = args[idx]
-                elif idx == 3:
-                    self.y = args[idx]
+        """assign argument to each attribute"""
+        if len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            elif len(args) >= 2:
+                self.size = args[1]
+            elif len(args) >= 3:
+                self.x = args[2]
+            elif len(args) >= 4:
+                self.y = args[3]
         else:
-            if len(kwargs) > 0:
-                key = kwargs.keys()
-                for i in key:
-                    if i == 'id':
-                        self.id = kwargs['id']
-                    elif i == 'size':
-                        self.size = kwargs['size']
-                    elif i == 'x':
-                        self.x = kwargs['x']
-                    elif i == 'y':
-                        self.y = kwargs['y']
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
-    # Return a dictionary representation of the square
     def to_dictionary(self):
-        """returns the dictionary representation of a square"""
+        """return the dictionary representation of a Square"""
         return {"id": self.id, "x": self.x, "size": self.size, "y": self.y}
