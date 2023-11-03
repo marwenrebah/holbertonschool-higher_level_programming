6a3e9f42ed5b6959b7568c47f3ab2a4504976d38
marwenrebah <6863@holbertonstudents.com>
2023-11-02 22:31:44 -1100

    Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" Square class"""
+"""Square class"""
+
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """inherits of rectangle class"""
+    """square inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """"instantiation"""
+        """instantiation"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Returns string info about this square."""
+        """string representation"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
 
-    # Getter and setter methods for the 'width' and 'height' property
+    # Getter and setter methods for the 'size' property
     @property
     def size(self):
         """get_method"""
@@ -27,3 +28,40 @@ class Square(Rectangle):
         """set_method"""
         self.width = value
         self.height = value
+
+    # Update the attributes of the square using either args or kwargs
+    def update(self, *args, **kwargs):
+        """Update"""
+
+        if args and len(args) != 0:
+            for idx in range(len(args)):
+                if not idx:
+                    self.id = args[idx]
+                elif idx == 1:
+                    self.size = args[idx]
+                elif idx == 2:
+                    self.x = args[idx]
+                elif idx == 3:
+                    self.y = args[idx]
+        else:
+            if len(kwargs) > 0:
+                key = kwargs.keys()
+                for i in key:
+                    if i == 'id':
+                        self.id = kwargs['id']
+                    elif i == 'size':
+                        self.size = kwargs['size']
+                    elif i == 'x':
+                        self.x = kwargs['x']
+                    elif i == 'y':
+                        self.y = kwargs['y']
+
+    # Return a dictionary representation of the square
+    def to_dictionary(self):
+        """returns the dictionary representation of a square"""
+
+        return {
+            'id': self.id,
+            'x': self.x,
+            'size': self.width,
+            'y': self.y}
