cd8ad1185e735c74ae392f1491a81fd24d903dca
marwenrebah <6863@holbertonstudents.com>
2023-11-24 02:31:46 -1100

    First add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
 -- Script that inserts a new row in the table first_table (database hbtn_0c_0) in my MySQL server.
-INSERT INTO first_table (
-    id = 89,
-    name = 'Best School'
-)
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
