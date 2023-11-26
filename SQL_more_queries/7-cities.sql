eff4059811a57a90d3c3b233fbfbbd5388961499
marwenrebah <6863@holbertonstudents.com>
2023-11-26 06:43:52 -1100

    Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on my MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE table IF NOT EXISTS cities(
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    PRIMARY KEY(id),
+    state_id INT NOT NULL,
+    FOREIGN KEY(state_id),
+    name VARCHAR(256) NOT NULL
+)
