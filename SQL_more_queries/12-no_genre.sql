6182c12921157534502058d7e88999da3d18ef3b
marwenrebah <6863@holbertonstudents.com>
2023-11-26 07:30:59 -1100

    No genre

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 RIGHT OUTER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows_genres.genre_id IS NULL
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
