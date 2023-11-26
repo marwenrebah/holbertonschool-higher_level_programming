c7e1057211daff685c738b790ce50201d0583335
marwenrebah <6863@holbertonstudents.com>
2023-11-26 06:57:12 -1100

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- Script that lists all cities contained in the database hbtn_0d_usa.
-SELECT C.id, C.name, S.name AS state
-FROM cities c
-JOIN states s ON C.state_id = S.id
+SELECT C.id, C.name, S.name
+FROM cities C
+INNER JOIN States S
+ON C.state_id = S.id
 ORDER BY C.id ASC;
