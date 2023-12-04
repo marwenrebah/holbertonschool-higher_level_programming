b8ace6f25b5860ac75e56c83c82d48eab616af86
marwenrebah <6863@holbertonstudents.com>
2023-12-04 00:11:17 -1100

    Square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,58 +1,49 @@
 #!/usr/bin/python3
-"""Write the class Square that inherits from Rectangle"""
+'''Module for Square class.'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """this is the square class"""
+    '''A Square class.'''
 
     def __init__(self, size, x=0, y=0, id=None):
-        """use all attributes of Rectangle"""
+        '''Constructor.'''
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """[Square] (<id>) <x>/<y> - <size>"""
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+        '''Returns string info about this square.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """retrieve  the value of size"""
+        '''Size of this square.'''
         return self.width
 
     @size.setter
-    def size(self, size):
-        """assign size to width and height"""
-        if not isinstance(size, int):
-            raise TypeError("width must be an integer")
-        elif size <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.width = size
-        self.height = size
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        '''Internal method that updates instance attributes via */**args.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
 
     def update(self, *args, **kwargs):
-        """assign argument to each attribute"""
-        if len(args) != 0:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
-        else:
-            for k, v in kwargs.items():
-                if k == "id":
-                    self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
 
     def to_dictionary(self):
-        """return the dictionary representation of a Square"""
-        return {"id": self.id, "x": self.x, "size": self.size, "y": self.y}
+        '''Returns dictionary representation of this class.'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
