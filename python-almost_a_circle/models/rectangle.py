37bb2ac344994f63812ee33bf1503278a8e9ff46
marwenrebah <6863@holbertonstudents.com>
2023-12-04 00:16:10 -1100

    Rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -56,7 +56,7 @@ class Rectangle(Base):
 
     def validate_integer(self, name, value, eq=True):
         '''Method for validating the value.'''
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if eq and value < 0:
             raise ValueError("{} must be >= 0".format(name))
